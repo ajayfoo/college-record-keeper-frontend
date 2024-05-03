8f0c8421b001f754ff485658a24e1bf07ef605d5
Ajay K <113184147+ajayfoo@users.noreply.github.com>
2024-05-03 15:05:50 +0530

    build!: use esm for config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,56 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import { resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
-  entry: './src/app.js',
-  mode: 'development',
-  devtool: 'inline-source-map',
+const isProduction = process.env.NODE_ENV == 'production';
+const stylesHandler = 'style-loader';
+const config = {
+  entry: {
+    workspace: './src/app.js',
+    login: './src/login/index.js',
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    path: resolve(import.meta.dirname, 'dist'),
   },
   devServer: {
-    static: './',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
+    open: false,
+    host: 'localhost',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/template.html',
+      filename: 'workspace.html',
+      template: './src/templates/workspace.html',
+      chunks: ['workspace'],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'login.html',
+      template: './src/templates/login.html',
+      chunks: ['login'],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+        resolve: { fullySpecified: false },
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+};
+
+export default () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
