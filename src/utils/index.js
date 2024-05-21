1b83c329827f56902ec841a6d476f6ec9ae08a2b
Ajay K <113184147+ajayfoo@users.noreply.github.com>
2024-05-21 12:54:13 +0530

    feat: show student report pdf in a new tab

--- src/utils/index.js
+++ src/utils/index.js
@@ -51,11 +51,8 @@ const downloadReportPdf = async (id) => {
   if (!response.ok) throw new Error(response.statusText);
   const blob = await response.blob();
   const url = window.URL.createObjectURL(blob);
-  const anchorEle = document.createElement('a');
-  anchorEle.href = url;
-  anchorEle.download = 'hello.pdf';
-  anchorEle.click();
   console.log(url);
+  window.open(url, '_blank');
 };
 export {
   getHttpEndpointForForm,
